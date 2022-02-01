e17329fcfae80f3f5bba252ee2d38454b52f5344
amcmax1 <amcmax1@protonmail.com>
2022-02-01 17:01:40 +0200

    (initial config:) type checking strict disabled, dockerfile for server app and server-db connection configured

---
+++ docker.dockerfile
@@ -0,0 +1,13 @@
+FROM node:14.17.1 as build
+ARG BUILD_CONTEXT
+
+WORKDIR /base
+COPY package.json .
+COPY yarn.lock .
+COPY ./packages/$BUILD_CONTEXT/package.json packages/$BUILD_CONTEXT/
+RUN yarn install
+COPY ./packages/$BUILD_CONTEXT packages/$BUILD_CONTEXT
+RUN yarn build:$BUILD_CONTEXT
+EXPOSE 3333
+RUN yarn global add nodemon
+ENTRYPOINT ["yarn", "server-dev"]
--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,10 +1,34 @@
 import express from "express";
 import 'dotenv/config'
 
-const app = express();
+require('dotenv').config()
 
-app.get('/',(req, res)=>{
-  res.json({"message":"Hello World!"});
+const { Pool } = require('pg')
+
+const app = express();
+app.use(express.json())
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+const connectionString = `postgres://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`;
+
+const pool = new Pool({
+  connectionString: connectionString,
+  ssl: isProduction,
+})
+
+app.get('/actions', (req, res) => {
+  pool.query('SELECT author FROM git_actions', (error, results) => {
+    if (error) {
+      throw error
+    }
+    res.status(200).json(results.rows)
+  })
+});
+
+app.post('/githooks', (req, res) => {
+  const action = req.body.stringify
+  res.json(action);
 });
 
 app.listen(3333);
